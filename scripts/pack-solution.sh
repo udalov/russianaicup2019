3b07a71ee5b3df9b6ab26bcad61a4a59bdefcd59
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2019-12-20 17:48:36 +0100

    Remove obsolete workaround for solution packing

--- scripts/pack-solution.sh
+++ scripts/pack-solution.sh
@@ -1,33 +1,12 @@
 #!/usr/bin/env bash
 
 shopt -s globstar
+set -e
 
 target=$(realpath out/solution.zip)
-tmp=$(realpath out/solution-tmp)
-base=$(realpath raic-2019/clients/cpp)
 
 mkdir -p $(dirname target)
 cd src
 
-rm -rf cmake-build-debug $target $tmp
+rm -rf cmake-build-debug $target
 zip $target **/*.cpp **/*.hpp **/*.h CMakeLists.txt >/dev/null
-
-<< EOF
-mkdir -p $tmp/model
-cp $base/Debug.* $tmp/
-cp $base/Stream.* $tmp/
-cp $base/TcpStream.* $tmp/
-cp $base/model/PlayerMessageGame.* $tmp/model/
-cp $base/model/PlayerView.* $tmp/model/
-cp $base/model/ServerMessageGame.* $tmp/model/
-cp $base/model/Versioned.* $tmp/model/
-sed 's|game.units|game.world.units|' $base/main.cpp > $tmp/main.cpp
-sed 's|result.data =|// result.data =|' $base/model/PlayerMessageGame.cpp > $tmp/model/PlayerMessageGame.cpp
-sed 's|game\.|// game.|' $base/model/PlayerView.cpp > $tmp/model/PlayerView.cpp
-sed 's|innerValue =|// innerValue =|' $base/model/Versioned.cpp > $tmp/model/Versioned.cpp
-echo "#pragma once" > $tmp/model/Vec2Double.hpp
-echo "#pragma once" > $tmp/model/Vec2Float.hpp
-
-cd $tmp
-zip $target **/*.cpp **/*.hpp >/dev/null
-EOF
